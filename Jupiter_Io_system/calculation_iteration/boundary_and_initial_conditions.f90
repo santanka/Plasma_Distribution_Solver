63d209f17d533d9897ce70c93e3d1546e857a0b0
santanka <mkrock1062@gmail.com>
2022-06-07 17:37:10 +0900

    constant, conditionの設定

---
+++ Jupiter_Io_system/calculation_iteration/boundary_and_initial_conditions.f90
@@ -0,0 +1,49 @@
+module boundary_and_initial_conditions
+    use constant_in_the_simulation
+
+    implicit none
+
+    !------------------
+    ! initial condition
+    !------------------
+
+    integer, parameter :: initial_fix_grid = 117
+    integer, parameter :: initial_min_grid_1 = 74
+    integer, parameter :: initial_min_grid_2 = real_grid_number + 1 - initial_min_grid_1
+
+    integer, parameter :: initial_grid_ionophere_middle_1 = 11
+    ! 1 ~ initial_grid_ionophere_middle_1 - 1   initial_electrostatic_potential_ionosphere
+    
+    integer, parameter :: initial_grid_middle_magnetosphere_1 = 27
+    ! initial_grid_ionosphere_middle_1 ~ initial_grid_middle_magnetosphere_1 - 1    initial_electrostatic_potential_middle
+
+    integer, parameter :: initial_grid_middle_magnetosphere_2 = real_grid_number + 2 - initial_grid_middle_magnetosphere_1
+    ! initial_grid_middle_magnetosphere_1 ~ initial_grid_middle_magnetosphere_2 - 1    initial_electrostatic_potential_magnetosphere
+  
+    integer, parameter :: initial_grid_ionophere_middle_2 = real_grid_number + 2 - initial_grid_ionophere_middle_1
+    ! initial_grid_middle_magnetosphere_2 ~ initial_grid_ionophere_middle_2 - 1     initial_electrostatic_potential_middle
+    ! initial_grid_ionophere_middle_2 ~ real_grid_number    initial_electrostatic_potential_ionosphere
+
+    double precision, parameter :: initial_electrostatic_potential_ionosphere = 3d4     ![V] = [kg m2 s-3 A-1]
+    double precision, parameter :: initial_electrostatic_potential_middle = 2.6d4       ![V] = [kg m2 s-3 A-1]
+    double precision, parameter :: initial_electrostatic_potential_magnetosphere = 0d0  ![V] = [kg m2 s-3 A-1]
+
+
+    !-------------------
+    ! boundary condition
+    !-------------------
+
+    character(len=128) :: boundary_file = '../conditions/boundary_conditions_1.csv'
+    integer, parameter :: boundary_series_number = 10
+    integer, parameter :: boundary_ionosphere_1_variable_species = 1
+    integer, parameter :: boundary_ionosphere_2_variable_species = 3
+    integer, parameter :: boundary_magnetosphere_variable_species = 6
+
+
+    !------------
+    ! result file
+    !------------
+
+    character(len=128) :: result_file = '../results/result_number_density_1.csv'
+
+end module boundary_and_initial_conditions    
---
+++ Jupiter_Io_system/calculation_iteration/constant_parameter.f90
@@ -0,0 +1,16 @@
+module constant_parameter
+    implicit none
+
+    !------------------------------------
+    ! mathematical and physical constants
+    !------------------------------------
+
+    double precision, parameter :: pi = 4d0*atan(1d0)
+    double precision, parameter :: elementary_charge = 1.602176634d-19  ![C] = [s A]
+    double precision, parameter :: speed_of_light = 2.99792458d8    ![m s-1]
+    double precision, parameter :: magnetic_constant = 1.25663706212d-6     ![kg m s-2 A-2]
+    double precision, parameter :: electric_constant = 1d0 / magnetic_constant / speed_of_light**2d0    ![kg-1 m-3 s4 A2]
+    double precision, parameter :: constant_of_gravitation = 6.67430d-11    ![kg-1 m3 s-2]
+    double precision, parameter :: electron_mass = 9.1093837015d-31     ![kg]
+
+end module constant_parameter
